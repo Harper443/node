1040df940d052746b831e8ed3562611310ad1102
Harper <caaptaain4plaaneet5@gmail.com>
2024-03-12 08:44:16 +0800

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+/** 
+ * Abvos node
+ * @module abv-node
+ * thor --amount 1000 --messages 100 sock://localhost:8080/abv
+ */
+"use strict";
+
+const Node = require('./lib/Node.js');
+
+module.exports = Node;
